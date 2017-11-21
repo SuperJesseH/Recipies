b17e3271a387cf565332f229aff2d60ee7b0b7e3
J <>
2017-11-20 21:30:29 -0500

    Mels Matcha added

---
+++ MelaniesMatcha.py
@@ -0,0 +1,22 @@
+# all variables stored as teaspoons(US)
+
+from sys import argv
+
+if len(argv) >= 1:
+	file, servings = argv
+else:
+	servings = 1
+
+matcha = float(servings) * 3
+turmeric = float(servings)
+cinnamon = float(servings) / 4
+coco_oil = float(servings) * 3
+
+
+print(f"""
+	Melanie's Matcha:
+	{servings} servings requires:
+	{round(coco_oil / 3, 1)} tablespoons of Coconut Oil ({round(coco_oil / 48.69221, 2)} cups)
+	{round(matcha / 3, 1) } tablespoons of Matcha powder ({round(matcha / 48.69221, 2)} cups)
+	{round(turmeric / 3, 1)} tablespoons of Turmeric
+	{round(cinnamon, 2)} teaspoons of Cinnamon """)
